43ccd4a14ec4ad05cfb3e011cfa2755beebf873d
Alexander <hottabbe@gmail.com>
2018-10-13 12:54:17 +0300

    Update first_comment.py

--- first_comment.py
+++ first_comment.py
@@ -1,7 +1,7 @@
 # vkscripts
 # Fcomment
 # Оставляет "первонахи" в любом паблике
-# 0.12
+# 0.2
 ###
 
 import vk
@@ -9,26 +9,29 @@ import sys
 import random
 import time
 import os
+import threading
 
 group_id = ''
 comments = []
+threads = []
 
 
-def idle():
+def idle(group):
     isClear = False
     try:
-        post = api.wall.get(owner_id='-%i' % group_id, count=1)['items'][0]['id']
+        post = api.wall.get(owner_id='-%i' % group, count=1)['items'][0]['id']
     except IndexError:
         if not isClear:
             print('Стена пустая,жду постов...')
         isClear = True
         post = 0
+    print('Запущено ожидание постов в группе id%s' % group)
     while True:
         try:
-            last_post = api.wall.get(owner_id='-%i' % group_id, count=1)['items'][0]['id']
+            last_post = api.wall.get(owner_id='-%i' % group, count=1)['items'][0]['id']
             if post != last_post:
-                api.wall.createComment(owner_id='-%i' % group_id, post_id=last_post, message=random.choice(comments))
-                print('https://vk.com/wall-%s_%s - Есть комментарий' % (group_id, last_post))
+                api.wall.createComment(owner_id='-%i' % group, post_id=last_post, message=random.choice(comments))
+                print('https://vk.com/wall-%s_%s - Есть комментарий' % (group, last_post))
             post = last_post
             time.sleep(0.7)
         except KeyboardInterrupt:
@@ -39,14 +42,18 @@ def idle():
             isClear = True
 
 
-if type(group_id) is not str:
+if len(group_id) > 0:
     if len(comments) > 0:
         api = vk.API(
             vk.AuthSession(app_id=5821493, user_login=input('login: '), user_password=input('password:'),
                            scope='groups,wall'),
             lang='ru', v='5.85')
         api.stats.trackVisitor()
-        idle()
+        for group in group_id:
+            t = threading.Thread(target=idle, args=(group,))
+            t.start()
+            threads.append(t)
+            time.sleep(1)
     else:
         print('Комментарии не заполнены\n'
               'Вводите комментарии и нажимайте Enter\n'
@@ -55,18 +62,29 @@ if type(group_id) is not str:
         while True:
             text = input('==>')
             if len(text) == 0:
-                with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'r', encoding='utf-8') as file:
+                with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'r',
+                          encoding='utf-8') as file:
                     new = file.read().replace("comments = []", 'comments = %s' % str(comments_))
-                with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'w', encoding='utf-8') as file:
+                with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'w',
+                          encoding='utf-8') as file:
                     file.write(new)
                 sys.exit(0)
             else:
                 comments_.append(text)
 else:
     print('Группа не заполнена\n'
-          'Вводите id группы и нажмите Enter')
-    with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'r', encoding='utf-8') as file:
-        new = file.read().replace('group_id = \'\'', 'group_id = %s' % input('==>'))
-    with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'w', encoding='utf-8') as file:
-        file.write(new)
-    sys.exit(0)
+          'Вводите id групп и нажимайте Enter\n'
+          'Когда закончите - нажмите Enter еще раз')
+    groups = []
+    while True:
+        text = input('==>')
+        if len(text) == 0:
+            with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'r',
+                      encoding='utf-8') as file:
+                new = file.read().replace('group_id = []', 'group_id = %s' % str(groups))
+            with open('%s/first_comment.py' % os.path.dirname(os.path.abspath(__file__)), 'w',
+                      encoding='utf-8') as file:
+                file.write(new)
+            sys.exit(0)
+        else:
+            groups.append(text)
